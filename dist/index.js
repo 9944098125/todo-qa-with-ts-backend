add1d91bf87c85705a3b3c07b85cb927d1a2941b
Srinivas Akella <akellasrinivas9944@gmail.com>
2025-01-04 00:50:48 +0530

    asf

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const express_session_1 = __importDefault(require("express-session"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const qa_1 = __importDefault(require("./routes/qa"));
@@ -25,6 +26,18 @@ app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.set("trust proxy", 1); // you need to add this
+app.use((0, express_session_1.default)({
+    secret: process.env.SECRET_TOKEN,
+    resave: false,
+    saveUninitialized: false,
+    proxy: true, // this is optional it depend which server you host, i am not sure about Heroku if you need it or not
+    cookie: {
+        secure: "auto", // this will set to false on developement, but true on Heroku since is https so this setting is required
+        maxAge: 10000, // 10 sec for testing
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", //by default in developement this is false if you're in developement mode
+    },
+}));
 // use the routes here
 app.use("/api/auth", auth_1.default);
 app.use("/api/qa", qa_1.default);
